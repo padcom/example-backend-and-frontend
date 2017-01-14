ab66bf4d7770f5fb6885ff77bcb706be634a1538
Matthias Hryniszak <padcom@gmail.com>
2017-01-14 13:45:03 +0100

    Added SpringLoaded to get automatic class reloading

--- run.sh
+++ run.sh
@@ -4,5 +4,11 @@
 # Forego: https://dl.equinox.io/ddollar/forego/stable
 #
 
+# Install springloaded to get automatic class reloading
+mvn dependency:get -DgroupId=org.springframework -DartifactId=springloaded -Dversion=1.2.6.RELEASE
+
+# Build the project
 mvn install
+
+# Run the project
 forego start
